b9a0fd26c7214480c4483e6d16a5e6acc14030f4
suyuan87 <m13013609156@163.com>
2024-06-06 14:15:35 +0800

    change file

--- device.mk
+++ device.mk
@@ -22,72 +22,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 ENABLE_AB := true
 ENABLE_VIRTUAL_AB := true
 
-PRODUCT_PACKAGES += update_engine \
-    update_engine_client \
-    update_verifier \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
-
-PRODUCT_PACKAGES += \
-  update_engine_sideload
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
-
 # Board
-BOARD_SHIPPING_API_LEVEL := 31
-BOARD_API_LEVEL := 31
+PRODUCT_TARGET_VNDK_VERSION := 33
 PRODUCT_SHIPPING_API_LEVEL := 31
-SHIPPING_API_LEVEL := 31
-
-# Boot/kernel Console enabled
-TARGET_CONSOLE_ENABLED := true
-
-# Build
-BUILD_BROKEN_DUP_RULES := true
-RELAX_USES_LIBRARY_CHECK := true
-
-# Crypto
+	
+# Boot Control HAL
 PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
+    bootctrl.taro.recovery \
+    android.hardware.boot@1.2-impl-qti.recovery
 
 # Dynamic partition Handling
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
-PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery \
-	fastbootd
-
-# f2fs utilities
-PRODUCT_PACKAGES += \
-    sg_write_buffer \
-    f2fs_io \
-    check_f2fs
-
-# Health HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl
-
-# Platform
-TARGET_BOARD_PLATFORM := taro
-TARGET_BOOTLOADER_BOARD_NAME := taro
-QCOM_BOARD_PLATFORMS += taro
+TW_INCLUDE_FASTBOOTD := true
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
-
-# Suppot to compile recovery without msm headers
-TARGET_HAS_GENERIC_KERNEL_HEADERS := true
-
-# Userdata checkpoint
-PRODUCT_PACKAGES += \
-    checkpoint_gc
